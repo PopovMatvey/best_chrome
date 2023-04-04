755f4ead69223a0725fd60a6c6109bc030da7565
матвей попов <matyha550@gmail.com>
2023-04-04 09:44:18 +0300

    front v 0.4.1

--- src/assets/pages/OurWorks/index.tsx
+++ src/assets/pages/OurWorks/index.tsx
@@ -10,7 +10,30 @@ export function OurWorks() {
             <div className="out-works-container">
                 <div className="out-works-container_beginner-screen beginner-screen">
                     <SlideMenu />
-                    <h1>Наши работы</h1>
+                    <div className="out-works-container_beginner-screen_container">
+                        <h1>Наши работы</h1>
+                        <div className="out-works-container_beginner-screen_text">
+                            <span>
+                                Вот вам яркий пример современных тенденций —
+                                синтетическое тестирование прекрасно подходит
+                                для реализации новых предложений.
+                            </span>
+                            <span>
+                                Каждый из нас понимает очевидную вещь: сложившаяся
+                                структура организации способствует повышению качества
+                                новых предложений.
+                            </span>
+                            <span>
+                                С другой стороны, глубокий уровень погружения обеспечивает
+                                широкому кругу (специалистов) участие в формировании направлений
+                                прогрессивного развития.
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div className="out-works-container_beginner-screen_portfolio">
+                    <h3>Портфолио</h3>
+                    <span>Скоро здесь будет портфолио...</span>
                 </div>
             </div>
         </>
--- src/assets/pages/Services/index.tsx
+++ src/assets/pages/Services/index.tsx
@@ -15,8 +15,8 @@ export function Services() {
                         <h1>Услуги</h1>
                         <div className="services-container_beginner-screen_assortiment">
                             {
-                                arrayTypeServices.map((element) => (
-                                    <div className="services-container_beginner-screen_assortiment_item">
+                                arrayTypeServices.map((element , i) => (
+                                    <div className="services-container_beginner-screen_assortiment_item" key={i++}>
                                         <img src={element.imageTypeService} alt="Изображение типа услуг" />
                                         <h3>{element.nameTypeService}</h3>
                                     </div>
