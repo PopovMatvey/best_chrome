d16051d43548c1462dcff804d30d48ee7ebfc56d
матвей попов <matyha550@gmail.com>
2023-05-25 17:08:15 +0300

    develop_app v 0.5.1

--- client/src/assets/pages/Learning/index.tsx
+++ client/src/assets/pages/Learning/index.tsx
@@ -21,44 +21,64 @@ export function Learning() {
                         <h1>Обучение</h1>
                         <div className="learning-container_beginner-screen_text">
                             <span>
-                                Вот вам яркий пример современных тенденций —
-                                синтетическое тестирование прекрасно подходит
-                                для реализации новых предложений. Каждый из нас понимает
-                                очевидную вещь: сложившаяся структура организации способствует
-                                повышению качества новых предложений.
+                                Проводим обучение по технологии, собственная разработка зеркального покрытия и своя линейка материалов от базового до финишного покрытия.
+                            </span>
+                            <span>
+                                Обучаем желающих как уже знакомых с химической металлизацией, но по какой-то причине не получается, так и тех, кто хочет начать свой бизнес с нуля, иметь дополнительный зароботок или как основной.
+                            </span>
+                            <span>
+                                Обучение проходит по алгоритму, отработанному годами. Если вы будете придерживаться нашей технологии, то ваше зеркальное покрытие не будет иметь желтоватый оттенок, муары, коричневые подтёки и прочий брак на поверхности.
+                            </span>
+                            <span>
+                                Наши ученики получают советы, рекомендации от выбора и обустройства помещения, до сфер направлений бизнеса, где наша технология позволяет принести наибольшую прибыль!
+                            </span>
+                            <span>
+                                Ученик после прохождения обучения не останется один, наш технолог по телефону (с 10 до 18 по МСК), проконсультирует, если возникнут какие-то вопросы!
+                            </span>
+                            <span>
+                                Также у нас вы можете приобрести наборы серебрения для самостоятельного нанесения, оборудование, установку для получения дистиллированной воды, на которой работают наши растворы, установку для получения дистиллированной воды, на которой работают наши растворы, установку по очистке воздуха от конденсата и масел, выходящих с компрессора.
+                            </span>
+                            <span>
+                                PS. Обучение может проходить как в нашем тех. центре, так и дистанционно, это всё обсуждается по телефону или WhatsApp.
+                            </span>
+                            <span>
+                                Работаем по всей России!
+                            </span>
+                            <span>
+                                Наши ученики всегда получают Результат!
                             </span>
                         </div>
-                        <div className="learning-container_beginner-screen_benifits">
-                            <h3>Преимущества обучения у нас:</h3>
-                            <div className="learning-container_beginner-screen_benifits_container">
-                                <div className="learning-container_beginner-screen_benifits_item">
-                                    <div className="learning-container_beginner-screen_benifits_item_image">
-                                        <img src={hends} alt="Иконка преимущества" />
-                                    </div>
-                                    <h6>
-                                        Востребованная
-                                        профессия
-                                    </h6>
-                                </div>
-                                <div className="learning-container_beginner-screen_benifits_item">
-                                    <div className="learning-container_beginner-screen_benifits_item_image">
-                                        <img src={time} alt="Иконка преимущества" />
-                                    </div>
-                                    <h6>
-                                        Быстрое
-                                        обучение
-                                    </h6>
-                                </div>
-                                <div className="learning-container_beginner-screen_benifits_item">
-                                    <div className="learning-container_beginner-screen_benifits_item_image">
-                                        <img src={sertificate} alt="Иконка преимущества" />
-                                    </div>
-                                    <h6>
-                                        Получение
-                                        сертификата
-                                    </h6>
-                                </div>
+                    </div>
+                </div>
+                <div className="learning-container_beginner-screen_benifits">
+                    <h3>Преимущества обучения у нас:</h3>
+                    <div className="learning-container_beginner-screen_benifits_container">
+                        <div className="learning-container_beginner-screen_benifits_item">
+                            <div className="learning-container_beginner-screen_benifits_item_image">
+                                <img src={hends} alt="Иконка преимущества" />
                             </div>
+                            <h6>
+                                Востребованная
+                                профессия
+                            </h6>
+                        </div>
+                        <div className="learning-container_beginner-screen_benifits_item">
+                            <div className="learning-container_beginner-screen_benifits_item_image">
+                                <img src={time} alt="Иконка преимущества" />
+                            </div>
+                            <h6>
+                                Быстрое
+                                обучение
+                            </h6>
+                        </div>
+                        <div className="learning-container_beginner-screen_benifits_item">
+                            <div className="learning-container_beginner-screen_benifits_item_image">
+                                <img src={sertificate} alt="Иконка преимущества" />
+                            </div>
+                            <h6>
+                                Получение
+                                сертификата
+                            </h6>
                         </div>
                     </div>
                 </div>
